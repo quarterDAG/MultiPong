f1aea47a219a05fd76074ca264d5c0a50b60ff64
quarterDAG <yamcohen@gmail.com>
2023-09-18 00:24:15 +0300

    added main menu scene

---
+++ Scripts/Main Menu.gd
@@ -0,0 +1,33 @@
+extends Control
+
+var peer = ENetMultiplayerPeer.new()
+@export var player_scene : PackedScene
+
+
+func _on_host_pressed():
+	peer.create_server(123)
+	multiplayer.multiplayer_peer = peer
+	multiplayer.peer_connected.connect(add_player)
+	add_player()
+	
+
+
+func _on_join_pressed():
+	peer.create_client(127.0.0.1, 123)
+	multiplayer.multiplayer_peer = peer
+
+func add_player(id = 1):
+	var player = player_scene.instantiate()
+	player.name = str(id)
+	call_deferred("add_child", player)
+	
+func exit_game(id):
+	multiplayer.peer_disconnected.connect(del_player)
+	del_player(id)
+
+	
+func del_player(id):
+	rpc("_del_player", id)
+	
+@rpc("any_peer", "call_local") func _del_player(id):
+	get_node(str(id)).queue_free()
--- Scripts/Paddle.gd
+++ Scripts/Paddle.gd
@@ -3,15 +3,13 @@ extends CharacterBody2D
 # Speed of the paddle
 @export var speed : float = 400
 
+func _enter_tree():
+	set_multiplayer_authority(name.to_int())
+
 # The function _physics_process runs every physics frame; this is where we'll put the movement code.
 func _physics_process(_delta):
-	var input_direction = Vector2(
-		Input.get_action_strength("move_right") - Input.get_action_strength("move_left"), 0
-	)
-	
-
-
-	velocity = input_direction * speed
-	
-	move_and_slide()
+	if is_multiplayer_authority():
+		var input_direction = Vector2(Input.get_action_strength("move_right") - Input.get_action_strength("move_left"), 0)
+		velocity = input_direction * speed
+		move_and_slide()
 	
